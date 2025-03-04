43c11702fbcb319bf0ce708eb622235fc3ce0775
gekishou <gonfuentes112@gmail.com>
2025-03-04 11:25:34 -0300

    refactored for expressjs

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const http = require("http");
 const path = require("path");
 const { readFile } = require("node:fs/promises");
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => serve(res, "/views/index.html"))
+app.get("/about", (req, res) => serve(res, "/views/about.html"))
+app.get("/contact-me", (req, res) => serve(res, "/views/contact-me.html"))
+app.use((req, res, next) => {
+  serve(res, "/views/404.html");
+});
+
+
 
 async function serve(res, viewName) {
   const viewPath = path.join(__dirname, viewName);
@@ -15,29 +26,32 @@ async function serve(res, viewName) {
   }
 }
 
-const server = http.createServer((req, res) => {
-  const myUrl = new URL(req.url, `http://${req.headers.host}`);
-  let viewName;
-  switch (myUrl.pathname) {
-    case ("/"):
-        viewName = "/views/index.html";
-        serve(res, viewName);        
-        break;
-    case ("/about"):
-        viewName = "/views/about.html";
-        serve(res, viewName);        
-        break;
-    case ("/contact-me"):
-        viewName = "/views/contact-me.html";
-        serve(res, viewName);        
-        break;
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server listening on : http://localhost:${PORT}`))
+
+// const server = http.createServer((req, res) => {
+//   const myUrl = new URL(req.url, `http://${req.headers.host}`);
+//   let viewName;
+//   switch (myUrl.pathname) {
+//     case ("/"):
+//         viewName = "/views/index.html";
+//         serve(res, viewName);        
+//         break;
+//     case ("/about"):
+//         viewName = "/views/about.html";
+//         serve(res, viewName);        
+//         break;
+//     case ("/contact-me"):
+//         viewName = "/views/contact-me.html";
+//         serve(res, viewName);        
+//         break;
   
-    default:
-        viewName = "/views/404.html";
-        serve(res, viewName);   
-        break;
-  }
+//     default:
+//         viewName = "/views/404.html";
+//         serve(res, viewName);   
+//         break;
+//   }
 
-});
+// });
 
-server.listen(8080, () => console.log("server running"));
+// server.listen(8080, () => console.log("server running"));
