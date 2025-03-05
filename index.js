105f4888ac7c4197f8b0e40cc9b7f4f9516cd279
gekishou <gonfuentes112@gmail.com>
2025-03-04 23:45:18 -0300

    add author router

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const http = require("http");
-const path = require("path");
-const { readFile } = require("node:fs/promises");
-const express = require('express');
+const express = require("express");
 const app = express();
+const authorRouter = require("./routes/authorRouter");
+
+app.use("/author", authorRouter);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", {root: __dirname})
-})
+  res.sendFile("./views/index.html", { root: __dirname });
+});
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", {root: __dirname})
-})
+  res.sendFile("./views/about.html", { root: __dirname });
+});
 app.get("/contact-me", (req, res) => {
-  res.sendFile("./views/contact-me.html", {root: __dirname})
-})
-
-app.get("/contact", (req, res) => {
-  res.redirect("contact-me")
-})
-app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", {root: __dirname})
+  res.sendFile("./views/contact-me.html", { root: __dirname });
 });
 
+app.get("/contact", (req, res) => {
+  res.redirect("contact-me");
+});
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
 
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Server listening on : http://localhost:${PORT}`)
+);
 
 // async function serve(res, viewName) {
 //   const viewPath = path.join(__dirname, viewName);
@@ -36,29 +39,26 @@ app.use((req, res) => {
 //   }
 // }
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server listening on : http://localhost:${PORT}`))
-
 // const server = http.createServer((req, res) => {
 //   const myUrl = new URL(req.url, `http://${req.headers.host}`);
 //   let viewName;
 //   switch (myUrl.pathname) {
 //     case ("/"):
 //         viewName = "/views/index.html";
-//         serve(res, viewName);        
+//         serve(res, viewName);
 //         break;
 //     case ("/about"):
 //         viewName = "/views/about.html";
-//         serve(res, viewName);        
+//         serve(res, viewName);
 //         break;
 //     case ("/contact-me"):
 //         viewName = "/views/contact-me.html";
-//         serve(res, viewName);        
+//         serve(res, viewName);
 //         break;
-  
+
 //     default:
 //         viewName = "/views/404.html";
-//         serve(res, viewName);   
+//         serve(res, viewName);
 //         break;
 //   }
 
