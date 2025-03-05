70fdb37574521b2f6d1b58464cb2a0797e7cbcde
gekishou <gonfuentes112@gmail.com>
2025-03-04 21:36:50 -0300

    fix: 404 page correctly served

--- index.js
+++ index.js
@@ -18,23 +18,23 @@ app.get("/contact", (req, res) => {
   res.redirect("contact-me")
 })
 app.use((req, res) => {
-  res.status(404).sendFile("./views/contact-me.html", {root: __dirname})
+  res.status(404).sendFile("./views/404.html", {root: __dirname})
 });
 
 
 
-async function serve(res, viewName) {
-  const viewPath = path.join(__dirname, viewName);
-  try {
-    const contents = await readFile(viewPath);
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = viewName === "/views/404.html" ? 404 : 200;
-    res.se(contents);
-  } catch (err) {
-    res.statusCode = 500;
-    res.end('Server Error');
-  }
-}
+// async function serve(res, viewName) {
+//   const viewPath = path.join(__dirname, viewName);
+//   try {
+//     const contents = await readFile(viewPath);
+//     res.setHeader('Content-Type', 'text/html');
+//     res.statusCode = viewName === "/views/404.html" ? 404 : 200;
+//     res.se(contents);
+//   } catch (err) {
+//     res.statusCode = 500;
+//     res.end('Server Error');
+//   }
+// }
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server listening on : http://localhost:${PORT}`))
