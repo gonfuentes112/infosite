8bcc4ac48b58557150399af24cd1f14e7a7b7238
gonzalo_fuentes <fubagg@kyndryl.com>
2025-03-06 10:32:34 -0300

    feat: add stylesheet

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use("/", indexRouter);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use((req, res) => {
   res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
